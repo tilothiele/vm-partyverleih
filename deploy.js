11071dfd98429a51855bd3e3ef5a71fa17fd9c95
Tilo Thiele <tilo.thiele@emessages.de>
2024-05-21 23:14:31 +0200

    1euro server ist umgezogen

--- deploy.js
+++ deploy.js
@@ -5,7 +5,7 @@ const { Client } = require('node-scp')
 console.log("deploy...")
 
 Client({
-    host: '212.132.101.21',
+    host: '194.164.54.189',
     port: 22,
     username: 'ansible',
     agent: process.env.SSH_AUTH_SOCK,
@@ -13,11 +13,16 @@ Client({
     // privateKey: fs.readFileSync('./key.pem'),
     // passphrase: 'your key passphrase',
   }).then(client => {
-    client.uploadDir('./public', '/home/ansible/www/vm-partyverleih.de')
-          .then(response => {
-            console.log('done')
-            client.close() // remember to close connection after you finish
-          })
-          .catch(error => {})
+    console.log('create directory')
+    client.mkdir('/srv/nginx/www/vm-partyverleih.de')
+    return client
+  }).then(client => {
+    console.log('copy content folder')
+    client.uploadDir('./public', '/srv/nginx/www/vm-partyverleih.de')
+    .then(response => {
+        console.log('done')
+        client.close() // remember to close connection after you finish
+      })
+      .catch(error => {})
   }).catch(e => console.log(e))
 
